2478cafd8eedd655becd62115ff11889c5ce0c16
Luiza Carvalho <luiza.silveira@ccc.ufcg.edu.br>
2015-04-23 15:25:42 -0300

    Quando um elemneto é adicionado a textbox fica vazia

--- dinamica/js/my_jquery_functions.js
+++ dinamica/js/my_jquery_functions.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
     $("#submeter1").click(function(){
     	var assunto=$("#aprender").val();
+    	$("#aprender").val("");
         $("#lista-aprender li:first").append("<li>"+assunto+"</li>");
     });
+    $
     $("#submeter2").click(function(){
     	var assunto1=$("#aprendi").val();
+    	$("#aprendi").val("");
         $("#lista-aprendi li:first").append("<li>"+assunto1+"</li>");
     });
 });
