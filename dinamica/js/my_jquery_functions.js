bad46cf8811a2057247f87375090ab1553a3107c
Luiza Carvalho <luiza.silveira@ccc.ufcg.edu.br>
2015-04-24 00:01:26 -0300

    Adiciona botões ao adicionar elementos as listas, mudança do tamanho da fonte

--- dinamica/js/my_jquery_functions.js
+++ dinamica/js/my_jquery_functions.js
@@ -2,12 +2,22 @@ $(document).ready(function(){
     $("#submeter1").click(function(){
     	var assunto=$("#aprender").val();
     	$("#aprender").val("");
-        $("#lista-aprender li:first").append("<li>"+assunto+"</li>");
+        $("#lista-aprender").append("<font size='5'><li>"+assunto+"</li></font>");
+        var b= $('<input type="button" class="btn btn-default btn-sm"  id:"ex" value="excluir"/>');
+         $("#lista-aprender").append(b);
+        var b1= $('<input type="button" class="btn btn-default btn-sm"  id:"mov" value="mover"/>');
+         $("#lista-aprender").append(b1);
     });
-    $
     $("#submeter2").click(function(){
     	var assunto1=$("#aprendi").val();
     	$("#aprendi").val("");
-        $("#lista-aprendi li:first").append("<li>"+assunto1+"</li>");
+        $("#lista-aprendi").append("<font size='5'><li>"+assunto1+"</li></font>");
+        var b2= $('<input type="button" class="btn btn-default btn-sm"  id:"ex" value="excluir"/>');
+         $("#lista-aprendi").append(b);
+        var b3= $('<input type="button" class="btn btn-default btn-sm"  id:"mov" value="mover"/>');
+         $("#lista-aprendi").append(b1);
+    });
+    $("#ex").click(function(){
+    	$("#lista-aprender").remove();
     });
 });
