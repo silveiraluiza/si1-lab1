962c924f9c7ea1241d06077aca0e4ba12334d896
Luiza Carvalho <luiza.silveira@ccc.ufcg.edu.br>
2015-04-24 13:30:34 -0300

    Deleta e move os elementos entre as listas, porém com problemas

--- dinamica/js/my_jquery_functions.js
+++ dinamica/js/my_jquery_functions.js
@@ -1,23 +1,39 @@
 $(document).ready(function(){
     $("#submeter1").click(function(){
-    	var assunto=$("#aprender").val();
-    	$("#aprender").val("");
-        $("#lista-aprender").append("<font size='5'><li>"+assunto+"</li></font>");
-        var b= $('<input type="button" class="btn btn-default btn-sm"  id:"ex" value="excluir"/>');
-         $("#lista-aprender").append(b);
-        var b1= $('<input type="button" class="btn btn-default btn-sm"  id:"mov" value="mover"/>');
-         $("#lista-aprender").append(b1);
+        var assunto=$("#aprender").val();
+        $("#aprender").val("");
+        if (assunto != "" ){
+             $("#lista-aprender").append("<font size='5'><li><p>"+assunto+
+                "</p></li></font>"+'<button id="ex" class="btn btn-small btn-danger">Remover</button>'+
+                "    "+'<button id="mov" class="btn btn-small btn-warning">Mover!</button>');
+        }
     });
     $("#submeter2").click(function(){
-    	var assunto1=$("#aprendi").val();
-    	$("#aprendi").val("");
-        $("#lista-aprendi").append("<font size='5'><li>"+assunto1+"</li></font>");
-        var b2= $('<input type="button" class="btn btn-default btn-sm"  id:"ex" value="excluir"/>');
-         $("#lista-aprendi").append(b);
-        var b3= $('<input type="button" class="btn btn-default btn-sm"  id:"mov" value="mover"/>');
-         $("#lista-aprendi").append(b1);
+        var assunto1=$("#aprendi").val();
+        $("#aprendi").val("");
+        if (assunto1 != ""){
+             $("#lista-aprendi").append("<font size='5'><li>"+assunto1+
+                "</li></font>"+'<button id="ex" class="btn btn-small btn-danger">Remover</button>'+
+                "    "+'<button id="mov1" class="btn btn-small btn-warning">Mover!</button>');
+        }
     });
-    $("#ex").click(function(){
-    	$("#lista-aprender").remove();
-    });
-});
+     $(document).on("click", "#mov", function(){
+      var temp =$(this).parent().text();
+      $("#lista-aprendi").append("<font size='5'><li><p>"+temp+
+                "</p></li></font>"+'<button id="ex" class="btn btn-small btn-danger">Remover</button>'+
+                "    "+'<button id="mov" class="btn btn-small btn-warning">Mover!</button>');
+      $(this).parent().remove();
+
+     });
+     $(document).on("click", "#ex", function(){
+            $(this).parent().remove();
+            $(this).remove();
+        });
+    $(document).on("click", "#mov1", function(){
+      var temp =$(this).parent().text();
+      $("#lista-aprender").append("<font size='5'><li><p>"+temp+
+                "</p></li></font>"+'<button id="ex" class="btn btn-small btn-danger">Remover</button>'+
+                "    "+'<button id="mov" class="btn btn-small btn-warning">Mover!</button>');
+      $(this).parent().remove();
+  });
+ });
